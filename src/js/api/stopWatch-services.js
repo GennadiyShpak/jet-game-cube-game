6ca02e6ad2d2f3aecb83ae314adcdefba756931c
Gennadiy <borchuga@gmail.com>
2020-12-09 23:31:13 +0200

    timer fix

--- src/js/api/stopWatch-services.js
+++ src/js/api/stopWatch-services.js
@@ -1,37 +1,37 @@
 export default class StopWatch {
-    constructor({onTick}) {
-        this.startTime =60;
-        this.intervalId = null;
-        this.onTick = onTick;
-    }
+  constructor({ onTick }) {
+    this.startTime = 60;
+    this.intervalId = null;
+    this.onTick = onTick;
+  }
 
-    start() {
-        this.isActive = true;
-        this.intervalId = setInterval(()=>{
-        this.startTime = this.startTime - 1; 
-        const time = this.pad(this.startTime);
-        this.onTick(time)
-        }, 100) 
-    }
+  start() {
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      this.startTime = this.startTime - 1;
+      const time = this.pad(this.startTime);
+      this.onTick(time);
+    }, 1000);
+  }
 
-    stop() {
-        clearTimeout(this.intervalId);
-        this.isActive = false;
-    }
+  stop() {
+    clearTimeout(this.intervalId);
+    this.isActive = false;
+  }
 
-    pause() {
-        clearTimeout(this.intervalId);
-        this.isActive = false;
-    }
+  pause() {
+    clearTimeout(this.intervalId);
+    this.isActive = false;
+  }
 
-    get () {
-       return this.time;
-    }
+  get() {
+    return this.time;
+  }
 
-    set (newTime) {
-        this.time = newTime;
-    }
-    pad(value) {
-        return String(value).padStart(2,'0');
-      }
+  set(newTime) {
+    this.time = newTime;
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 }
